419bfcc73e65193360c6752af9ef3801bad40679
Rabat <rabatkarabek8@gmail.com>
2024-05-09 21:46:37 +0500

    improved and added new classes

--- src/Main.java
+++ src/Main.java
@@ -22,23 +22,24 @@ public class Main {
 
         BST<Integer, String> bst = new BST<>();
 
-        bst.put(50, "A");
-        bst.put(30, "AA");
+        bst.put(30, "B"); // This replaces bst.put(50, "A")
+        bst.put(40, "C"); // Changed from bst.put(30, "AA")
         bst.put(70, "AAA");
-        bst.put(20, "AAAA");
-        bst.put(40, "AAAAA");
-        bst.put(60, "AAAAAA");
-        bst.put(80, "AAAAAAA");
+        bst.put(20, "D"); // Changed from bst.put(20, "AAAA")
+        bst.put(50, "E"); // Changed from bst.put(40, "AAAAA")
+        bst.put(60, "F"); // Remains the same
+        bst.put(80, "G"); // Remains the same
 
-        System.out.println(bst.get(40));
-        System.out.println(bst.get(60));
+        System.out.println("Value for key 40: " + bst.get(40)); // Expected to return "C"
+        System.out.println("Value for key 60: " + bst.get(60)); // Expected to return "F"
 
-        bst.delete(30);
+        // Testing delete operation
+        bst.delete(40); // Adjusted the key to delete from 30 to 40
+        System.out.println("Contains key 40? " + bst.contains(40)); // Expected to be false after deletion
 
-        System.out.println(bst.contains(30));
-
+        // Loop through all keys in the BST
         for (Integer key : bst) {
-            System.out.println(key + " " + bst.get(key));
+            System.out.println("Key: " + key + ", Value: " + bst.get(key));
         }
     }
     public static String generateRandomString(int length) {
